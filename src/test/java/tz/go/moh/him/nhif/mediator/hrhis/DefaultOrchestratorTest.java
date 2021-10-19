f9c996854e5312800cb847af24f01cd76d094539
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2021-10-19 06:29:24 +0300

    Added tests

--- src/test/java/tz/go/moh/him/nhif/mediator/hrhis/DefaultOrchestratorTest.java
+++ src/test/java/tz/go/moh/him/nhif/mediator/hrhis/DefaultOrchestratorTest.java
@@ -4,18 +4,36 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.JavaTestKit;
-import java.util.Collections;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.*;
 import org.openhim.mediator.engine.MediatorConfig;
 import org.openhim.mediator.engine.messages.FinishRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
+import org.openhim.mediator.engine.testing.MockLauncher;
+import org.openhim.mediator.engine.testing.TestingUtils;
+import tz.go.moh.him.nhif.mediator.hrhis.mock.MockDestination;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
 
 public class DefaultOrchestratorTest {
+    /**
+     * Represents the configuration.
+     */
+    protected static MediatorConfig configuration;
 
-    static ActorSystem system;
+    /**
+     * Represents the system actor.
+     */
+    protected static ActorSystem system;
 
     @BeforeClass
     public static void setup() {
@@ -28,8 +46,70 @@ public class DefaultOrchestratorTest {
         system = null;
     }
 
+    /**
+     * Runs initialization before each class execution.
+     */
+    @BeforeClass
+    public static void beforeClass() {
+        try {
+            configuration = loadConfig(null);
+            system = ActorSystem.create();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Loads the mediator configuration.
+     *
+     * @param configPath The configuration path.
+     * @return Returns the mediator configuration.
+     */
+    public static MediatorConfig loadConfig(String configPath) {
+        MediatorConfig config = new MediatorConfig();
+
+
+        try {
+            if (configPath != null) {
+                Properties props = new Properties();
+                File conf = new File(configPath);
+                InputStream in = FileUtils.openInputStream(conf);
+                props.load(in);
+                IOUtils.closeQuietly(in);
+
+                config.setProperties(props);
+            } else {
+                config.setProperties("mediator.properties");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        config.setName(config.getProperty("mediator.name"));
+        config.setServerHost(config.getProperty("mediator.host"));
+        config.setServerPort(Integer.parseInt(config.getProperty("mediator.port")));
+        config.setRootTimeout(Integer.parseInt(config.getProperty("mediator.timeout")));
+
+        config.setCoreHost(config.getProperty("core.host"));
+        config.setCoreAPIUsername(config.getProperty("core.api.user"));
+        config.setCoreAPIPassword(config.getProperty("core.api.password"));
+
+        config.setCoreAPIPort(Integer.parseInt(config.getProperty("core.api.port")));
+        config.setHeartbeatsEnabled(true);
+
+        return config;
+    }
+
+    /**
+     * Runs initialization before each test execution.
+     */
     @Before
-    public void setUp() throws Exception {
+    public void before() {
+        List<MockLauncher.ActorToLaunch> actorsToLaunch = new LinkedList<>();
+
+        actorsToLaunch.add(new MockLauncher.ActorToLaunch("http-connector", MockDestination.class));
+
+        TestingUtils.launchActors(system, configuration.getName(), actorsToLaunch);
     }
 
     @After
@@ -39,8 +119,7 @@ public class DefaultOrchestratorTest {
     @Test
     public void testMediatorHTTPRequest() throws Exception {
         new JavaTestKit(system) {{
-            final MediatorConfig testConfig = new MediatorConfig("nhif-mediator-hrhis", "localhost", 3104);
-            final ActorRef defaultOrchestrator = system.actorOf(Props.create(DefaultOrchestrator.class, testConfig));
+            final ActorRef defaultOrchestrator = system.actorOf(Props.create(DefaultOrchestrator.class, configuration));
 
             MediatorHTTPRequest POST_Request = new MediatorHTTPRequest(
                     getRef(),
@@ -50,7 +129,7 @@ public class DefaultOrchestratorTest {
                     "http",
                     null,
                     null,
-                    "/mediator",
+                    "/nhif",
                     "test message",
                     Collections.<String, String>singletonMap("Content-Type", "text/plain"),
                     Collections.<Pair<String, String>>emptyList()
